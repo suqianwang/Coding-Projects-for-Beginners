6e70f3196b95437bbebcb1422c8c5044624b7e93
Suqian Wang <wangsuqian123@tamu.edu>
2018-09-11 18:38:04 -0500

    Add files via upload

---
+++ Database_API/Database.h
@@ -0,0 +1,36 @@
+//
+// Database.h
+// Created by Dario Avendano on 9/24/2017.
+// Modified by Suqian Wang
+// Team 44: Alex Kaiser, Dario Avendano, Suqian Wang
+//
+
+#ifndef DATABASE_DATABASE_H_
+#define DATABASE_DATABASE_H_
+#include "Table.h"
+#include <stdlib.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+class Database {
+public:
+    //constructor
+    Database();
+    
+    // table operations
+    void add_table(Table table, string table_name);
+    void drop_table(string table_name);
+    vector<string> list_table();
+    vector<Table> get_tables();
+    
+    // query operation
+    Table Query(string select_command , string from_command , string where_command);
+    
+    
+};
+
+
+#endif /*DATABASE_DATABASE_H_*/
